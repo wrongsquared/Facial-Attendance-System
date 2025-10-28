2f076763784a46a8e729489bb52748d678e7a10e
wrongsquared <rongxiangng@outlook.com>
2025-10-28 21:23:39 +0800

    Initial Commit 2: The Committing
    In this commit, you will find a bare frontend created with vite-react, the backend set up for fastapi, alembic and postgresql, the commands to create a local database are in main.py.

---
+++ backend/db.py
@@ -0,0 +1,112 @@
+from __future__ import annotations
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase, relationship
+from sqlalchemy import (
+    Enum as SQLAlchemyEnum,
+    ForeignKey,
+    DateTime,
+    Boolean,
+    String,
+    Text,
+    func,
+    text,
+)
+import datetime
+import enum
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class ConsultStatus(enum.Enum):
+    PENDING = "PENDING"
+    COMPLETED = "COMPLETED"
+    MISSED = "MISSED"
+
+class UserProfile(Base):
+    __tablename__ = "UserProfiles"
+    profileTypeID: Mapped[int] = mapped_column(primary_key=True)
+    users: Mapped[list["User"]] = relationship(back_populates="profileType")
+
+class User(Base):
+    __tablename__ = "Users"
+    __mapper_args__ = {"polymorphic_identity": "user", "polymorphic_on": "type"} 
+    type: Mapped[str]
+
+    userID: Mapped[int] = mapped_column(primary_key=True)
+
+    profileTypeID: Mapped[int] = mapped_column(ForeignKey("UserProfiles.profileTypeID"))
+    profileType: Mapped[UserProfile] = relationship(back_populates="users")
+
+    name: Mapped[str] = mapped_column(String(50))
+    username: Mapped[str] = mapped_column(String(25))
+    #This might change if the password will be stored as Alphanum
+    password: Mapped[str] = mapped_column(String(25))
+
+class Admin(User):
+    __tablename__ = "Admins"
+    __mapper_args__ = {"polymorphic_identity": "admin"}
+    adminID: Mapped[int] = mapped_column(ForeignKey("Users.userID"), primary_key = True)
+
+class Lecturer(User):
+    __tablename__ = "Lecturers"
+    __mapper_args__ = {"polymorphic_identity": "lecturer"}
+    lecturerID: Mapped[int] = mapped_column(ForeignKey("Users.userID"), primary_key = True)
+    modules: Mapped[list[LecMod]] = relationship(back_populates="lecturers")
+
+
+class Student(User):
+    __tablename__ = "Students"
+    __mapper_args__ = {"polymorphic_identity": "student"}
+    studentID: Mapped[int] = mapped_column(ForeignKey("Users.userID"),  primary_key = True)
+    course: Mapped[str] = mapped_column(String[50])
+    attendanceMinimum: Mapped[float]
+
+class StudentFace(Base):
+    __tablename__ = "Studentfaces"
+    StudentID: Mapped[int] = mapped_column(ForeignKey("Students.studentID"), primary_key = True)
+    #photo: havednt figure out what datatype it is
+
+class Attendance(Base):
+    __tablename__ = "Attendance"
+    attendanceID: Mapped[int]= mapped_column(primary_key=True)
+    lessonID: Mapped[int] = mapped_column(ForeignKey("Lessons.lessonID"))
+    studentID: Mapped[int] = mapped_column(ForeignKey("Students.studentID"))
+    attendance_check: Mapped[bool]
+    enter: Mapped[datetime.datetime]
+    leave: Mapped[datetime.datetime]
+
+class Module(Base):
+    __tablename__ = "Modules"
+    moduleID: Mapped[int] = mapped_column(primary_key=True)
+    moduleName: Mapped[str] = mapped_column(String(25))
+    moduleCode: Mapped[str] = mapped_column(String(8))
+    Lessons: Mapped[list[LecMod]] = relationship(back_populates="modules")
+
+class Lesson(Base):
+    __tablename__ = "Lessons"
+    lessonID: Mapped[int] = mapped_column(primary_key=True)
+
+    lecModID: Mapped[int] = mapped_column(ForeignKey("LecMods.lecModID"))
+    studentsID: Mapped[int] = mapped_column(ForeignKey("Students.studentID"))
+    lessontype: Mapped[str] = mapped_column(String[10])
+    startDateTime: Mapped[datetime.datetime]
+    endDateTime: Mapped[datetime.datetime]
+
+
+class LecMod(Base):
+    __tablename__ = "LecMods"
+    lecModID: Mapped[int] = mapped_column(primary_key= True)
+
+    lecturerID: Mapped[int] = mapped_column(ForeignKey("Lecturers.lecturerID"))
+    lecturers: Mapped[Lecturer] = relationship(back_populates="lessons")
+
+    moduleID: Mapped[int] = mapped_column(ForeignKey("Modules.moduleID"))
+    modules: Mapped[Module] = relationship(back_populates="Lessons")
+
+class StudentLesson(Base):
+    __tablename__ = "StudentLessons"
+    studentLessonsID: Mapped[int] = mapped_column(primary_key= True)
+
+    studentID: Mapped[int] = mapped_column(ForeignKey("Students.studentID"))
+    lecturerID: Mapped[int] = mapped_column(ForeignKey("Lecturers.lecturerID"))
