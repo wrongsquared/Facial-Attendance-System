32286687beb2a8879706bd3379a8d6c43acc8c89
wrongsquared <rongxiangng@outlook.com>
2025-12-16 16:44:52 +0800

    Minor Changes
    Gets most of the code out of main.py

--- backend/routers/studentDashboardRouter.py
+++ backend/routers/studentDashboardRouter.py
@@ -4,7 +4,12 @@ from sqlalchemy import and_, func, case, desc
 from datetime import datetime, timedelta
 from database.db_config import get_db
 from dependencies.deps import get_current_user_id
-from pdantic.schemas import StudentLessons, TodaysLessons, OverallLessonsResponse, AttendancePerModule, PreviousAttendances, WeeklyLesson
+from pdantic.schemas import( StudentLessons, 
+                            TodaysLessons, 
+                            OverallLessonsResponse,
+                            AttendancePerModule, 
+                            PreviousAttendances, 
+                            WeeklyLesson)
 from database.db import  Lesson, Module,  StudentModules, LecMod, AttdCheck
 
 
